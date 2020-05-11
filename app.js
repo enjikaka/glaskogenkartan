de6eba519c70b863821c150310a497b8a3ef8e4f
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2020-05-11 18:58:08 +0200

    Convert to Glaskogenkarta

--- app.js
+++ app.js
@@ -1,57 +1,82 @@
 import './components/lantmateriet-karta.js';
 import './components/reko-page.js';
 
-async function loadMarkersFromJSON({ map, path, color, fillColor }) {
-  const response = await fetch(path);
-  const items = await response.json();
+const overnightCabinIcon = L.icon({
+  iconUrl: 'img/overnight-cabin.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [32, 0]
+});
 
-  items.forEach(item => {
-    const circleMarker = L.circleMarker(item.coords, {
-      color,
-      fillColor,
-      fillOpacity: 0.5,
-      radius: 10,
-    });
+const leanToIcon = L.icon({
+  iconUrl: 'img/lean-to.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [32, 0]
+});
 
-    circleMarker.on('click', () => {
-      document.dispatchEvent(new CustomEvent('show:page', {
-        detail: {
-          pageId: btoa(item.coords)
-        }
-      }));
-    });
+const layByIcon = L.icon({
+  iconUrl: 'img/lay-by.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [32, 0]
+});
 
-    circleMarker.on('touchup', () => {
-      document.dispatchEvent(new CustomEvent('show:page', {
-        detail: {
-          pageId: btoa(item.coords)
-        }
-      }));
-    });
+const undefinedIcon = L.icon({
+  iconUrl: 'img/undefined.svg',
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
+  popupAnchor: [32, 0]
+});
 
-    circleMarker.addTo(map);
-  });
+function getIconForFeature (feature) {
+  if (feature.properties.type === 'OVERNIGHT_CABIN') {
+    return overnightCabinIcon;
+  }
+
+  if (feature.properties.type === 'LEAN_TO') {
+    return leanToIcon;
+  }
+
+  if (feature.properties.type === 'LAY_BY') {
+    return layByIcon;
+  }
+
+  return undefinedIcon;
 }
 
-const loadLocalFoodNodes = map => loadMarkersFromJSON({
+async function loadMarkersFromJSON({ map, path, color, fillColor }) {
+  const response = await fetch(path);
+  const json = await response.json();
+
+  const pointToLayer = (feature, latlng) => L.marker(latlng, { icon: getIconForFeature(feature) });
+
+  const markers = L.markerClusterGroup();
+
+  L.geoJSON(json, { pointToLayer }).addTo(markers);
+
+  markers.addTo(map);
+}
+
+const loadLayBys = map => loadMarkersFromJSON({
   map,
-  path: 'data/localFoodNodes.json',
-  color: 'var(--local-food-node-colour-darker)',
-  fillColor: 'var(--local-food-node-colour)'
+  path: 'data/layBy.json'
 });
 
-const loadRekoRings = map => loadMarkersFromJSON({
+const loadLeanTos = map => loadMarkersFromJSON({
   map,
-  path: 'data/rekorings.json',
-  color: 'var(--reko-colour-darker)',
-  fillColor: 'var(--reko-colour)'
+  path: 'data/leanTo.json'
 });
 
-async function loadMarkers () {
-  const { leafletMap: map } = document.querySelector('lantmateriet-karta');
+const loadOvernightCabins = map => loadMarkersFromJSON({
+  map,
+  path: 'data/overnightCabins.json'
+});
 
-  loadRekoRings(map);
-  loadLocalFoodNodes(map);
+async function loadMarkers (map) {
+  loadOvernightCabins(map);
+  loadLeanTos(map);
+  loadLayBys(map);
 }
 
 async function loadPage (event) {
@@ -60,7 +85,34 @@ async function loadPage (event) {
   rekoPage.setAttribute('page-id', event.detail.pageId);
 }
 
-document.addEventListener('map:ready', () => loadMarkers());
+document.addEventListener('map:ready', () => {
+  const { leafletMap: map } = document.querySelector('lantmateriet-karta');
+
+  const glaskogenBounds = new L.LatLngBounds([
+    [59.61628, 12.21927], // Top left
+    [59.36161, 12.60455] // Bottom right
+  ]);
+
+  const polylinePoints = [
+    glaskogenBounds.getNorthWest(),
+    glaskogenBounds.getNorthEast(),
+    glaskogenBounds.getSouthEast(),
+    glaskogenBounds.getSouthWest(),
+    glaskogenBounds.getNorthWest()
+  ];
+
+  L.polyline(polylinePoints, { color: '#BC5800' }).addTo(map);
+
+  map.setView([59.48048673863045, 12.404165267944338], 11);
+
+  loadMarkers(map);
+
+  map.on('click', function(ev){
+    const { lat, lng } = map.mouseEventToLatLng(ev.originalEvent);
+    console.log(JSON.stringify([lng, lat]));
+  });
+});
+
 document.addEventListener('show:page', event => loadPage(event));
 document.addEventListener('DOMContentLoaded', () => {
   if (document.location.hash.includes('/page/')) {
--- components/lantmateriet-karta.js
+++ components/lantmateriet-karta.js
@@ -32,6 +32,8 @@ class LantmaterietKarta extends Component {
   render () {
     return `
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css">
+      <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.css">
+      <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.4.1/dist/MarkerCluster.Default.css">
       <style>
       :host,
       #map {
